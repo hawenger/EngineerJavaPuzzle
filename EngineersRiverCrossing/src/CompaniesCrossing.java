d180e0257e93328de347da477db7b4316ae8e4d2
Ameco <hamecow@gmail.com>
2021-04-20 10:42:16 -0700

    Initial commit

---
+++ EngineersRiverCrossing/src/CompaniesCrossing.java
@@ -0,0 +1,12 @@
+// Put your name here.
+public class CompaniesCrossing {
+	
+	public static boolean isMoveOkay(int beforeBoat, int beforeA, int beforeB, int beforeC, 
+			int beforeD, int beforeE, int beforeF, int afterBoat, int afterA, int afterB, 
+			int afterC, int afterD, int afterE, int afterF) {
+	
+		//Your code goes here!
+		return false;
+	}
+
+}
